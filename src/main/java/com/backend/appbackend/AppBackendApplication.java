b7c96601e5c0ca303c2600435fca12a62dd143db
Tomas Brezinskas <brezinskas.tomas@gmail.com>
2019-03-03 20:31:49 +0200

    Bijau Dano, refactorinu koda.

--- src/main/java/com/backend/appbackend/AppBackendApplication.java
+++ src/main/java/com/backend/appbackend/AppBackendApplication.java
@@ -2,26 +2,10 @@ package com.backend.appbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.HashMap;
-
-@Controller
 @SpringBootApplication
 public class AppBackendApplication {
-
-	@RequestMapping("/hello")
-	@ResponseBody
-	public HashMap<String, String> returnHello() {
-		HashMap<String, String> map = new HashMap<>();
-		map.put("hello", "world");
-		return map;
-	}
-
-	public static void main(String[] args) {
-		SpringApplication.run(AppBackendApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(AppBackendApplication.class, args);
+    }
 }
---
+++ src/main/java/com/backend/appbackend/controller/MainController.java
@@ -0,0 +1,23 @@
+package com.backend.appbackend.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+
+@Controller
+public class MainController {
+
+    public MainController() {
+
+    }
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public HashMap<String, String> returnHello() {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("hello", "world");
+        return map;
+    }
+}
