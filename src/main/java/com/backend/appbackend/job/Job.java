4d924c082414dc980c553e9eb935ab6ebc6dfa7c
Tomas Brezinskas <brezinskas.tomas@gmail.com>
2019-03-04 21:46:47 +0200

    added db support. works locally, testing to see if it works in heroku.

---
+++ src/main/java/com/backend/appbackend/job/Job.java
@@ -0,0 +1,26 @@
+package com.backend.appbackend.job;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class Job {
+
+    private String name;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
