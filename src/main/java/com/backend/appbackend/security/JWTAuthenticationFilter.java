dcf66c12f5ad321ffc2815238007199885e0abec
Tomas Brezinskas <brezinskas.tomas@gmail.com>
2019-03-07 18:23:56 +0200

    Minor refactoring variables, removed unnecessary functionality.

--- src/main/java/com/backend/appbackend/security/JWTAuthenticationFilter.java
+++ src/main/java/com/backend/appbackend/security/JWTAuthenticationFilter.java
@@ -32,13 +32,13 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     public Authentication attemptAuthentication(HttpServletRequest req,
                                                 HttpServletResponse res) throws AuthenticationException {
         try {
-            User creds = new ObjectMapper()
+            User credentials = new ObjectMapper()
                     .readValue(req.getInputStream(), User.class);
 
             return authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(
-                            creds.getEmail(),
-                            creds.getPassword(),
+                            credentials.getEmail(),
+                            credentials.getPassword(),
                             new ArrayList<>())
             );
         } catch (IOException e) {
--- src/main/java/com/backend/appbackend/user/UserRepository.java
+++ src/main/java/com/backend/appbackend/user/UserRepository.java
@@ -1,10 +1,7 @@
 package com.backend.appbackend.user;
 
-import com.backend.appbackend.user.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface UserRepository extends MongoRepository<User, String> {
-    User findUserById(String id);
-
     User findUserByEmail(String email);
 }
