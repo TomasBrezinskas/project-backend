ace9eec4323c6e3cb896ca82f2ea6f633f7ed550
Tomas Brezinskas <brezinskas.tomas@gmail.com>
2019-03-13 14:55:47 +0200

    deleting client

--- src/main/java/com/backend/appbackend/client/ClientRepository.java
+++
@@ -1,7 +0,0 @@
-package com.backend.appbackend.client;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ClientRepository extends MongoRepository<Client, String> {
-    Client findClientById(String id);
-}
